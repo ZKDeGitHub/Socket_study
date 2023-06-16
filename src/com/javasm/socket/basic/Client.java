e61911e7162566c9f0d70873894ea4b829273c57
ZKDeGitHub <ke15290172726@gmail.com>
2023-06-16 17:20:27 +0800

    6.16

--- src/com/javasm/socket/basic/Client.java
+++ src/com/javasm/socket/basic/Client.java
@@ -1,4 +1,24 @@
 package com.javasm.socket.basic;
 
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.Socket;
+
 public class Client {
+    public static void main(String[] args) {
+
+
+        try {
+            // 创建Socket对象，指定IP地址和端口号
+            Socket socket = new Socket("127.0.0.1",8081);
+
+            // 数据交互
+            // 客户端：读取内容，输入流
+            DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+            System.out.println("客户端接收的内容" + dataInputStream.readUTF());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/com/javasm/socket/basic/Sever.java
+++ src/com/javasm/socket/basic/Sever.java
@@ -1,7 +1,9 @@
 package com.javasm.socket.basic;
 
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Sever {
     public static void main(String[] args) {
@@ -9,8 +11,27 @@ public class Sever {
 
             // 创建ServerSocket对象，指定端口号
             ServerSocket serverSocket = new ServerSocket(8081);
+            System.out.println("服务器端已开启，监听8081端口");
+
+            // 设置死循环-不断监听客户端的连接
+            while(true){
+
+                // 监听客户端的连接，是一个阻塞方法
+                Socket socket = serverSocket.accept();
+
+                // 获取来连接服务端的客户端
+                String hostAddress = socket.getInetAddress().getHostAddress();
+                System.out.println("未连接的客户端" + hostAddress);
+
+                // 数据交互
+                // 服务端：写出内容，输出流
+                DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+                dataOutputStream.writeUTF("你好，客户端" + hostAddress);
+
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
+
     }
 }
